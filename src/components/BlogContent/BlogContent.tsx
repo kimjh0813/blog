abbac07c53981d4ca65f2d857c4462e4e5270d0f
Kim jonghun <developerkimjh@gmail.com>
2024-08-09 14:34:28 +0900

    FEAT: add Code for mdxComponent

--- src/components/BlogContent/BlogContent.tsx
+++ src/components/BlogContent/BlogContent.tsx
@@ -1,10 +1,14 @@
 'use client';
 
+import { MDXComponents } from 'mdx/types';
 import { MDXRemote } from 'next-mdx-remote';
 
+import { Code } from '../ui';
 import * as T from './type';
 
-const components = {};
+const components: MDXComponents = {
+  code: ({ className, children }) => <Code className={className}>{children}</Code>,
+};
 
 export function BlogContent({ post }: T.BlogContentProps) {
   return <MDXRemote {...post.source} components={components} />;
