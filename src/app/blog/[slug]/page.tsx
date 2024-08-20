b8da7ffbfbe13a3a1f692f586d3c1f713e1248c2
Kim jonghun <developerkimjh@gmail.com>
2024-08-20 11:04:13 +0900

    FEAT: add 404 page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,8 +2,12 @@ import { getPost } from '@/util/getPost';
 
 import { BlogContent } from '@/components';
 
+import { notFound } from 'next/navigation';
+
 export default async function Blog({ params }: { params: { slug: string } }) {
   const post = await getPost(params.slug);
+
+  if (!post) return notFound();
 
   return <BlogContent post={post} />;
 }
--- src/util/getPost.ts
+++ src/util/getPost.ts
@@ -45,6 +45,12 @@ export const getPosts = async () => {
       });
     }
 
+    postsData.sort((a, b) => {
+      const dateA = new Date(a.metaData.updatedAt).getTime();
+      const dateB = new Date(b.metaData.updatedAt).getTime();
+      return dateB - dateA;
+    });
+
     return { postsData, categories: Array.from(categorySet) };
   } catch (error: any) {
     throw error;
@@ -53,11 +59,13 @@ export const getPosts = async () => {
 
 export const getPost = async (slug: string) => {
   try {
-    const post = await fs.readFile(path.join(process.cwd(), `/post/${slug}.mdx`), 'utf-8');
+    const posts = await fs.readdir('post', 'utf-8');
 
-    if (!post) {
-      throw new Error('No file found');
-    }
+    const slugPost = posts.filter(v => v === `${slug}.mdx`);
+
+    if (slugPost.length === 0) return undefined;
+
+    const post = await fs.readFile(path.join(process.cwd(), `/post/${slug}.mdx`), 'utf-8');
 
     const { data, content } = matter(post);
 
